b5b9640859c07daef66f377c9f5527ffc6b256a6
Muhammed Apak <muhampak25@gmail.com>
2024-03-28 02:50:54 +0300

    navbar font changed

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Navbar() {
-  return <div className="bg-gray-400 absolute">v1.0</div>;
+  return (
+    <div className="bg-gray-400 absolute w-full flex justify-between font-mono">
+      <h1>v1.0</h1>
+      <h1  >BluDFiRe corp.</h1>
+    </div>
+  );
 }
 
 export default Navbar;
