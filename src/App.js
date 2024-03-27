7d0a868d62b5f9e51907889e9229cbddd450970e
Muhammed Apak <muhampak25@gmail.com>
2024-03-28 02:45:30 +0300

    passing time and remaining time hide when timer not counting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Flex, Box, Heading, Text, Button } from "@chakra-ui/react";
+import Navbar from "./navbar/Navbar";
 
 const Counter = ({ title }) => {
-  const [count, setCount] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(90 * 60); // 90 dakika
   const [backgroundColor, setBackgroundColor] = useState("transparent");
   const [isRunning, setIsRunning] = useState(false);
 
@@ -11,7 +13,8 @@ const Counter = ({ title }) => {
   useEffect(() => {
     if (isRunning) {
       timer = setInterval(() => {
-        setCount((prevCount) => prevCount + 1);
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+        setRemainingTime((prevRemainingTime) => prevRemainingTime - 1);
       }, 1000);
     }
 
@@ -19,22 +22,29 @@ const Counter = ({ title }) => {
   }, [isRunning]);
 
   useEffect(() => {
-    if (count >= 3600) {
+    if (elapsedTime >= 3600) {
       setBackgroundColor("red");
       clearInterval(timer);
-    } else if (count >= 1800) {
+    } else if (elapsedTime >= 1800) {
       setBackgroundColor("yellow");
     }
-  }, [count]);
+  }, [elapsedTime]);
 
   const handleStartStop = () => {
     setIsRunning((prevState) => !prevState);
   };
 
   const handleReset = () => {
-    setCount(0);
+    setElapsedTime(0);
+    setRemainingTime(90 * 60);
     setIsRunning(false);
     setBackgroundColor("transparent");
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
   };
 
   return (
@@ -46,7 +56,8 @@ const Counter = ({ title }) => {
       borderRadius="lg"
       textAlign="center"
       className="this"
-      borderBottom={"2px solid aqua"}
+      borderRight={"5px solid aqua"}
+      borderLeft={"5px solid aqua"}
     >
       <Heading
         size="md"
@@ -54,21 +65,32 @@ const Counter = ({ title }) => {
       >
         {title}
       </Heading>
-      <Text fontSize="2xl" className="text-red-500 text-2xl font-bold">
-        {Math.floor(count / 60)}:
-        {count % 60 < 10 ? `0${count % 60}` : count % 60}
+      {isRunning && (
+        <>
+        <Text fontSize="2xl" className="text-red-200 text-lg font-bold font-mono">
+        Geçen Süre: {formatTime(elapsedTime)} Kalan Süre:{" "}
+        {formatTime(remainingTime)}
       </Text>
-      <Flex justify="space-evenly" mt="4">
+      </>
+      )}
+      
+      <Flex justify="space-evenly" mt="10">
         <Button
           onClick={handleStartStop}
           colorScheme={isRunning ? "red" : "green"}
           w="80px"
           mr="20"
-          className="flex justify-center items-center font-bold overflow-hidden"
+          border={"2px solid blue"}
+          className="flex justify-center items-center font-bold overflow-hidden bg-orange-400"
         >
           {isRunning ? "Duraklat" : "Başlat"}
         </Button>
-        <Button onClick={handleReset} colorScheme="blue" className="font-bold">
+        <Button
+          onClick={handleReset}
+          colorScheme="blue"
+          className="font-bold p-4 text-white bg-red-400"
+          border={"2px solid red"}
+        >
           Sıfırla
         </Button>
       </Flex>
@@ -78,15 +100,17 @@ const Counter = ({ title }) => {
 
 const App = () => {
   return (
-    <Flex justify="center" align="center" h="100vh" className="bg-gray-400 ">
+    <div>
+
+      <Navbar />
+    <Flex justify="center" align="center" h="100vh" className="bg-gray-400  ">
       <Flex
-        direction="column"
+        direction="row"
         align="center"
-        className=" my-20"
+        className=" my-20 "
         border={"4px solid aqua"}
-      >
-        <Heading className="font-bold text-lg mx-10 border-b-4 border-blue-500 text-blue-700">
-          {" "}
+        >
+        <Heading className="font-bold text-2xl   mx-10 border-b-4 border-blue-500 text-blue-700">
           Death Valley Monument
         </Heading>
         <Counter title="Dv-1" />
@@ -96,6 +120,7 @@ const App = () => {
         <Counter title="Dv-5" />
       </Flex>
     </Flex>
+        </div>
   );
 };
 
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="bg-gray-400">v1.0</div>;
+}
+
+export default Navbar;
